6d1a01686b3249c090f100cf63502a92d2714e25
tecknovice <tecknovice@gmail.com>
2020-05-12 10:21:58 +0700

    add servlet-filter

---
+++ servletfilter/src/main/java/demo/filters/RequestResponseLoggingFilter.java
@@ -0,0 +1,34 @@
+package demo.filters;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//@Component
+@Order(2)
+public class RequestResponseLoggingFilter implements Filter {
+    private final static Logger LOG = LoggerFactory.getLogger(RequestResponseLoggingFilter.class);
+    @Override
+    public void doFilter(
+            ServletRequest request,
+            ServletResponse response,
+            FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        LOG.info(
+                "Logging Request  {} : {}", req.getMethod(),
+                req.getRequestURI());
+        chain.doFilter(request, response);
+        LOG.info(
+                "Logging Response :{}",
+                res.getContentType());
+    }
+
+    // other methods
+}
